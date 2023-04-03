eb0d0199f84c7d12c84176ca7631db1f0949d26c
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2023-04-03 14:16:20 +0300

    HW8, fix using function

--- HW8/task_2.py
+++ HW8/task_2.py
@@ -4,9 +4,7 @@ set2 = {3, 7, 5, 6, 8, 10, 84, 69, 71}
 
 
 def unique_unit(set_1, set_2):
-    unique_set1 = set_1.difference(set_2)
-    unique_set2 = set_2.difference(set_1)
-    unique_set = unique_set1.union(unique_set2)
+    unique_set = set_1.symmetric_difference(set_2)
     return unique_set
 
 
