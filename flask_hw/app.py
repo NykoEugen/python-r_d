30d2b362ecec7c1dc351fcbc7baecae16dc49d6d
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2023-07-10 01:40:33 +0300

    HW flask_4. Add many func, work with sqlalchemy

--- flask_hw/app.py
+++ flask_hw/app.py
@@ -1,16 +1,31 @@
-from flask import Flask, json
+from flask import Flask
 import logging
+from dotenv import load_dotenv
 
+from flask_sqlalchemy import SQLAlchemy
 
-log_config = '%(asctime)s %(levelname)s in %(module)s: %(message)s'
-logging.basicConfig(format=log_config, level=logging.INFO)
+from config import AppConfig
 
+# log_config = '%(asctime)s %(levelname)s in %(module)s: %(message)s'
+# logging.basicConfig(format=log_config, level=logging.INFO)
+
+load_dotenv()
+
+db = SQLAlchemy()
 app = Flask(__name__)
-app.secret_key = 'secret key'
+app.logger.setLevel(logging.INFO)
+app.config.from_object(AppConfig)
+db.init_app(app)
+
 
 from views import *
+from models import *
 
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=app.config.get('HOST'),
+            port=app.config.get('PORT'),
+            debug=app.config.get('DEBUG'))
