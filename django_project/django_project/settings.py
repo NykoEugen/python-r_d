4954897fd3a9fe33f4b39400e0770ff33335965e
NykoEugen <121561222+NykoEugen@users.noreply.github.com>
2023-07-12 19:28:24 +0300

    First Django project/2

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,11 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+
+class AppConfig:
+    DEBUG = os.getenv('DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    ROOT_URLCONF = os.getenv('ROOT_URLCONF')
+    BASE_DIR = os.getenv('BASE_DIR')
+    WSGI_APPLICATION = os.getenv('WSGI_APPLICATION')
+
+
+conf = AppConfig()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
